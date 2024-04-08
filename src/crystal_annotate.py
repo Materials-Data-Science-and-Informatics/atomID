aa6e32f3d89e6365910ce25a35284a2b080922fe
Ninad Bhat <bhat.ninadmb@gmail.com>
2024-04-08 16:41:16 +0200

    Add dev packages

---
+++ src/crystal_annotate.py
@@ -0,0 +1,56 @@
+from ase.io import read as ase_read
+from pyscal3.operations.input import read_inputfile
+from pyscal3 import System
+
+def read_crystal_structure_file(filename, format=None):
+    """
+    Read a crystal file and return the pyscal atoms object
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file to read
+    format : str
+        The format of the file. If None, the format is guessed from the file extension
+
+    Returns
+    -------
+    crystal_structure : ase.Atoms
+        The crystal structure
+    system : pyscal.System
+        The pyscal system object
+
+    """
+
+    crystal_structure = ase_read(filename, format=format)
+
+    # Convert to pyscal atoms object
+    system = System()
+    read_inputfile(system=system, 
+                   filename=crystal_structure, 
+                   format='ase')
+
+    return crystal_structure, system
+
+
+def get_crystal_structure_using_cna(pyscal_system):
+    """
+    Get the crystal structure using adaptive common neighbor analysis
+    
+    Parameters
+    ----------
+    pyscal_system : pyscal.System
+        The pyscal system object
+        
+    Returns
+    -------
+    cna_results : dict
+        The results of the common neighbor analysis
+    """
+
+    cna_results = pyscal_system.analysis.common_neighbor_analysis()
+
+    return cna_results
+
+
+
