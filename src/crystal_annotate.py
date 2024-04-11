1cc8a395114e280e956d9b5d21e8883e5b7b5fef
Ninad Bhat <bhat.ninadmb@gmail.com>
2024-04-11 15:53:13 +0200

    Add example

--- src/crystal_annotate.py
+++ src/crystal_annotate.py
@@ -4,6 +4,11 @@ from typing import Tuple, Dict, Optional
 import ase
 from scipy.signal import find_peaks
 from math import sqrt
+from rdflib import Literal, XSD, URIRef, Namespace
+
+CMSO = Namespace("http://purls.helmholtz-metadaten.de/cmso/")
+PLDO = Namespace("http://purls.helmholtz-metadaten.de/pldo/")
+PODO = Namespace("http://purls.helmholtz-metadaten.de/podo/")
 
 
 def read_crystal_structure_file(
@@ -53,7 +58,7 @@ def get_crystal_structure_using_cna(pyscal_system: System) -> Dict[str, int]:
         The results of the common neighbor analysis
     """
 
-    cna_results = pyscal_system.analysis.common_neighbor_analysis()
+    cna_results = pyscal_system.analyze.common_neighbor_analysis()
 
     return dict(cna_results)
 
@@ -103,7 +108,7 @@ def find_lattice_parameter(
         If the lattice type is not supported.
     """
 
-    val, dist = crystal_system.get_calculator().radial_distribution_function(bins=500)
+    val, dist = crystal_system.calculate.radial_distribution_function(bins=500)
     peaks, _ = find_peaks(val, height=0)
 
     lattice_calculation = {
@@ -118,3 +123,87 @@ def find_lattice_parameter(
     lattice_constants = lattice_calculation[lattice_type](dist[peaks[0]])
 
     return lattice_constants
+
+
+def get_lattice_angle(lattice_type: str) -> Tuple[float, float, float]:
+    """
+    Get the lattice angles of the crystal structure
+
+    Parameters
+    ----------
+    crystal_structure : ase.Atoms
+        The crystal structure
+
+    Returns
+    -------
+    Tuple[float, float, float]
+        The lattice angles of the crystal structure
+    """
+    lattice_angle = {
+        "fcc": (90, 90, 90),
+        "bcc": (90, 90, 90),
+        "hcp": (90, 90, 120),
+    }
+
+    return lattice_angle[lattice_type]
+
+
+if __name__ == "__main__":
+    crystal_structure, system, kg = read_crystal_structure_file(
+        "/home/n.bhat/development/data/fcc/no_defects/Al/Al.cif", format="cif"
+    )
+    system.to_graph()
+    cna_results = get_crystal_structure_using_cna(system)
+    # Pick key with maximum value from cn_results
+    crystal_type = max(cna_results, key=cna_results.__getitem__)
+
+    volume = calculate_volume(crystal_structure)
+    if crystal_type != "other":
+        lattice_constants = find_lattice_parameter(system, crystal_type)
+        lattice_angles = get_lattice_angle(crystal_type)
+
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeParameter"),
+                CMSO.hasLength_x,
+                Literal(lattice_constants[0], datatype=XSD.float),
+            )
+        )
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeParameter"),
+                CMSO.hasLength_y,
+                Literal(lattice_constants[1], datatype=XSD.float),
+            )
+        )
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeParameter"),
+                CMSO.hasLength_z,
+                Literal(lattice_constants[2], datatype=XSD.float),
+            )
+        )
+
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeAngle"),
+                CMSO.hasAngle_alpha,
+                Literal(lattice_angles[0], datatype=XSD.float),
+            )
+        )
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeAngle"),
+                CMSO.hasAngle_beta,
+                Literal(lattice_angles[1], datatype=XSD.float),
+            )
+        )
+        kg.graph.add(
+            (
+                URIRef(f"{system._name}_LatticeAngle"),
+                CMSO.hasAngle_gamma,
+                Literal(lattice_angles[2], datatype=XSD.float),
+            )
+        )
+
+    kg.write("crystal_structure_test.ttl", format="ttl")
