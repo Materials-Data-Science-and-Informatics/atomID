e2bf68043c59bddc06c56f5ef3b5bf20c2db712c
Ninad Bhat <bhat.ninadmb@gmail.com>
2024-04-15 15:30:39 +0200

    add substitution defects

--- src/point_defect_analysis/wigner_seitz_method.py
+++ src/point_defect_analysis/wigner_seitz_method.py
@@ -24,7 +24,12 @@ def find_nearest_atom(atom: tuple, atom_positions: np.ndarray) -> Tuple[int, flo
     return nearest_index, distances[nearest_index]
 
 
-def analyze_defects(reference_positions: list, actual_positions: list) -> dict:
+def analyze_defects(
+    reference_positions: list,
+    actual_positions: list,
+    species_ref: list = [],
+    species_actual: list = [],
+) -> dict:
     """Analyze the lattice for vacancy and interstitial defects.
 
     Parameters
@@ -43,30 +48,48 @@ def analyze_defects(reference_positions: list, actual_positions: list) -> dict:
     """
     reference_positions = np.array(reference_positions)
     actual_positions = np.array(actual_positions)
-    usage_count = np.zeros(len(reference_positions))
-
+    atom_position_list = np.zeros(len(reference_positions))
+    substitution_list = np.zeros(len(reference_positions))
     # Analyze atom positions to detect vacancies and interstitials
     for actual in actual_positions:
         nearest_index, _ = find_nearest_atom(actual, reference_positions)
-        usage_count[nearest_index] += 1
+        if species_actual and species_ref:
+            if species_actual[nearest_index] == species_ref[0]:
+                atom_position_list[nearest_index] += 1
+            else:
+                substitution_list[nearest_index] += 1
 
     vacancies = [
         (i, tuple(pos))
         for i, pos in enumerate(reference_positions)
-        if usage_count[i] == 0
+        if atom_position_list[i] == 0 and substitution_list[i] == 0
     ]
+
     vacancy_count = len(vacancies)
     vacancy_fraction = round(vacancy_count / len(actual_positions), 3)
     interstitials = [
-        (i, tuple(pos)) for i, pos in enumerate(actual_positions) if usage_count[i] > 1
+        (i, tuple(pos))
+        for i, pos in enumerate(actual_positions)
+        if atom_position_list[i] > 1
     ]
     interstitial_count = len(interstitials)
     interstitial_fraction = round(interstitial_count / len(actual_positions), 3)
 
+    substitutions = [
+        (i, tuple(pos))
+        for i, pos in enumerate(reference_positions)
+        if substitution_list[i] > 0
+    ]
+    substitution_count = len(substitutions)
+    substitution_fraction = round(substitution_count / len(actual_positions), 3)
     return {
         "Vacancies": {"count": vacancy_count, "fraction": vacancy_fraction},
         "Interstitials": {
             "count": interstitial_count,
             "fraction": interstitial_fraction,
+        },
+        "Substitutions": {
+            "count": substitution_count,
+            "fraction": substitution_fraction,
         },
     }
