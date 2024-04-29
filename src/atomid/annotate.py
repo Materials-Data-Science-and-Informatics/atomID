5673c9ddb00f82a15b09cbd5f40c3e3d342eef16
Ninad Bhat <bhat.ninadmb@gmail.com>
2024-04-29 13:56:01 +0200

    Add docstrings

--- src/atomid/annotate.py
+++ src/atomid/annotate.py
@@ -18,7 +18,15 @@ class AnnotateCrystal:
         self.kg = ardf.KnowledgeGraph()
 
     def read_crystal_structure_file(self, data_file: str, format: str) -> None:
-        """Read the crystal structure file."""
+        """Read the crystal structure file.
+
+        Parameters
+        ----------
+        data_file : str
+            The name of the file to read
+        format : str
+            The format of the file. If None, the format is guessed from the file extension
+        """
         crystal_data = ase_read(data_file, format=format)
         kg = ardf.KnowledgeGraph()
         crystal_structure = ardf.System.read.file(
@@ -29,7 +37,11 @@ class AnnotateCrystal:
         self.system = crystal_structure
 
     def annotate_crystal_structure(self) -> None:
-        """Identify and annotate the crystal structure."""
+        """Identify and annotate the crystal structure.
+
+        This method identifies the crystal structure using Common Neighbour Analysis
+        and lattice constant using radial distribution function.
+        """
         crystal_structure = get_crystal_structure_using_cna(self.system)
 
         if crystal_structure != "others":
@@ -43,6 +55,20 @@ class AnnotateCrystal:
             )
 
     def identify_defects(self, reference_data_file: str, ref_format: str) -> dict:
+        """Identify defects in the crystal structure using the reference data file.
+
+        Parameters
+        ----------
+        reference_data_file : str
+            The name of the file to read
+        ref_format : str
+            The format of the file. If None, the format is guessed from the file extension
+
+        Returns
+        -------
+        defects : dict
+            A dictionary containing the vacancy and interstitial defects.
+        """
         actual_positions = self.system.atoms.positions
         ref_ase = ase_read(reference_data_file, format=ref_format)
         ref_positions = ref_ase.positions
@@ -54,6 +80,16 @@ class AnnotateCrystal:
         return defects
 
     def annotate_defects(self, reference_data_file: str, ref_format: str) -> None:
+        """Annotate defects in the crystal structure using the reference data file.
+
+        Parameters
+        ----------
+        reference_data_file : str
+            The name of the file to read
+        ref_format : str
+            The format of the file. If None, the format is guessed from the file extension
+
+        """
         defects = self.identify_defects(reference_data_file, ref_format)
 
         vacancies = defects.get("Vacancies", {"count": 0, "fraction": 0})
@@ -64,5 +100,13 @@ class AnnotateCrystal:
             )
 
     def write_to_file(self, filename: str, format: str = "ttl") -> None:
-        """Write the annotated system to a file."""
+        """Write the annotated system to a file.
+
+        Parameters
+        ----------
+        filename : str
+            The name of the file to write
+        format : str
+            The format of the file. If None, the format is guessed from the file extension
+        """
         self.kg.write(filename, format=format)
