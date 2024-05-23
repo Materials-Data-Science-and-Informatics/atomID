ee1fda89df219f70ec77c9b3c95c1c246a0426c3
Ninad <bhat.ninadmb@gmail.com>
2024-05-23 11:39:37 +0200

    Add triples for interstitial and substitutional impurities

--- src/atomid/annotate.py
+++ src/atomid/annotate.py
@@ -103,10 +103,24 @@ class AnnotateCrystal:
         defects = self.identify_defects(reference_data_file, ref_format, method)
 
         vacancies = defects.get("Vacancies", {"count": 0, "fraction": 0})
+        interstitials = defects.get("Interstitials", {"count": 0, "fraction": 0})
+        substitutions = defects.get("Substitutions", {"count": 0, "fraction": 0})
 
         if vacancies["count"] > 0:
             self.system.add_vacancy(
                 concentration=vacancies["fraction"], number=vacancies["count"]
+            )
+
+        if interstitials["count"] > 0:
+            self.system.add_triples_for_interstitial_impurities(
+                conc_of_impurities=interstitials["fraction"],
+                no_of_impurities=interstitials["count"],
+            )
+
+        if substitutions["count"] > 0:
+            self.system.add_triples_for_substitutional_impurities(
+                conc_of_impurities=substitutions["fraction"],
+                no_of_impurities=substitutions["count"],
             )
 
     def write_to_file(self, filename: str, format: str = "ttl") -> None:
